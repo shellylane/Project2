e8b4436827c81a11f2a3b5c348d1a50e7bd1bde9
Andrew Muscara <andrewmuscara@gmail.com>
2018-11-19 11:37:11 -0700

    finished front end site nav

--- models/seeds.sql
+++ models/seeds.sql
@@ -9,11 +9,11 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 );
 
-INSERT INTO Users (email, password,firstName, lastName, phoneNumber, role)
-VALUES ("andrewmuscara@gmail.com", "chillin", "andrew", "muscara", 4805555555, "coach");
+-- INSERT INTO Users (email, password,firstName, lastName, phoneNumber, role)
+-- VALUES ("andrewmuscara@gmail.com", "chillin", "andrew", "muscara", 4805555555, "coach");
 
-INSERT INTO Users (email, password,firstName, lastName, phoneNumber, role)
-VALUES ("shelly@gmail.com", "mtdew", "shelly", "lane", 4802745555, "parent");
+-- INSERT INTO Users (email, password,firstName, lastName, phoneNumber, role)
+-- VALUES ("shelly@gmail.com", "mtdew", "shelly", "lane", 4802745555, "parent");
 
-INSERT INTO Users (email, password,firstName, lastName, phoneNumber, role)
-VALUES ("darlene@aol.net", "forums", "darlene", "leslie", 6025555555, "parent");
+-- INSERT INTO Users (email, password,firstName, lastName, phoneNumber, role)
+-- VALUES ("darlene@aol.net", "forums", "darlene", "leslie", 6025555555, "parent");
